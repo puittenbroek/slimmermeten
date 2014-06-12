f6c182b17422267e54a1906fc517dfd3cd102e1b
Peter Uittenbroek <prmuittenbroek@gmail.com>
2014-06-12 21:46:43 +0200

    Some first admin stuff

--- slimmermeten/admin.py
+++ slimmermeten/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from slimmermeten.models import ElektricityReading, GasReading, PowerConsumption
 
-# Register your models here.
+
+class PowerConsumptionAdmin(admin.ModelAdmin):
+	list_display = ('date', 'power')
+
+class ElektricityReadingAdmin(admin.ModelAdmin):
+    list_display = ('date', 'tarief', 't1_reading', 't1_back_reading', 't2_stand', 't2_back_reading', )
+
+admin.site.register(ElektricityReading, ElektricityReadingAdmin)
+admin.site.register(GasReading)
+admin.site.register(PowerConsumption, PowerConsumptionAdmin)
