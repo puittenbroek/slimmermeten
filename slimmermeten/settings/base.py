bb30652d61d50288b591f0863c95317cd7b2dd48
Peter Uittenbroek <prmuittenbroek@gmail.com>
2014-06-20 21:58:33 +0200

    Not TZ

--- slimmermeten/settings/base.py
+++ slimmermeten/settings/base.py
@@ -114,7 +114,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False 
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
