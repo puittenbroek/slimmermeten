9d4f5fa9c74558da73ce175232b10782b948531c
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2014-06-20 15:50:00 +0200

    Timezone?

--- slimmermeten/settings/base.py
+++ slimmermeten/settings/base.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'Europe/Amsterdam'
 
 # List of finder classes that know how to find static files in
 # various locations.
