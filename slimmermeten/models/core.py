95769d18f764be0dbc905465b8e420b070dfe531
Peter Uittenbroek <prmuittenbroek@gmail.com>
2014-06-28 18:30:11 +0200

    Higher precision

--- slimmermeten/models/core.py
+++ slimmermeten/models/core.py
@@ -5,12 +5,12 @@ class ElektricityReading(models.Model):
     # Gives a tick per 10 seconds? We measure per minute
     date = models.DateTimeField('date of measurement')
     # T1 standen (normal + back to grid)
-    t1_reading = models.DecimalField(default=0,max_digits=12, decimal_places=2) #23:00-07:00
-    t1_back_reading = models.DecimalField(default=0,max_digits=12, decimal_places=2) #23:00-07:00
+    t1_reading = models.DecimalField(default=0,max_digits=12, decimal_places=3) #23:00-07:00
+    t1_back_reading = models.DecimalField(default=0,max_digits=12, decimal_places=3) #23:00-07:00
 
     # T2 standen (normal + back to grid)
-    t2_reading = models.DecimalField(default=0,max_digits=12, decimal_places=2) #07:00-23:00
-    t2_back_reading = models.DecimalField(default=0,max_digits=12, decimal_places=2) #07:00-23:00
+    t2_reading = models.DecimalField(default=0,max_digits=12, decimal_places=3) #07:00-23:00
+    t2_back_reading = models.DecimalField(default=0,max_digits=12, decimal_places=3) #07:00-23:00
 
     tarief = models.IntegerField(default=0) #0 of 1 (T1 of T2)
 
